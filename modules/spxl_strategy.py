0b9f8748343ad6a4a8091fcf23fb9e675f61a3c7
unlordlab <144677954+unlordlab@users.noreply.github.com>
2026-02-01 11:47:02 +0100

    Update spxl_strategy.py

--- modules/spxl_strategy.py
+++ modules/spxl_strategy.py
@@ -29,7 +29,7 @@ def render():
             st.download_button(
                 label="📄 Descargar Estrategia Completa (PDF)",
                 data=pdf_bytes,
-                file_name="assets/SPXL.pdf",
+                file_name="https://github.com/unlordlab/RSU-Terminal/assets/SPXL.pdfassets/SPXL.pdf",
                 mime="application/pdf"
             )
         else:
@@ -129,4 +129,5 @@ def render():
 # Para ejecutar la función si el script se corre directamente
 if __name__ == "__main__":
     render()
+
 

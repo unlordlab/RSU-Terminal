371cde8ffba58c8987bc80fe3f277baf56ff15ca
unlordlab <144677954+unlordlab@users.noreply.github.com>
2026-02-10 19:53:02 +0100

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,4 +1,10 @@
 # modules/__init__.py
+# Este archivo permite que Python reconozca la carpeta modules como un paquete
+
+# Importar todos los módulos para facilitar el acceso
+# from . import market
+# from . import canslim
+# from . import rsudb
 
 
 
